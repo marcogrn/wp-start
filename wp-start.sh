6b67f7b548de6304375313cd2fadfc9cdea6dbc2
Marco Guerini <marcoguerini@mail.com>
2018-04-27 21:57:19 +0200

    fix variables

--- wp-start.sh
+++ wp-start.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
-# setup variables
-PROJECT_NAME="domainname.com"
-WWW_PATH="/var/www/"
-WWW_USER="pvg"
-WWW_GROUP="www-data"
+# first make this script executable!
+# chmod +x wp-sart.sh
 
-echo Hello, write your project name:
-read PROJECT_NAME
+# the domain name is used for naming the project folder 
+# es. domainname.com
+echo write domain name:
+read DOMAIN_NAME
 
+# define the path where inizialize the project
+# es. /var/www/
 echo whrere is the www path?
 read WWW_PATH
 
+# define the owner user of the project
+# es. www-data
 echo who is the www user?
 read WWW_USER
 
+# define the www group
+# es. www-data
 echo what is the www group?
 read WWW_GROUP
 
-# create directory
-mkdir $WWW_PATH/$PROJECT_NAME
-mkdir $WWW_PATH/$PROJECT_NAME/log
-mkdir $WWW_PATH/$PROJECT_NAME/public_html
+# create project directory
+mkdir $WWW_PATH/$DOMAIN_NAME
+mkdir $WWW_PATH/$DOMAIN_NAME/log
+mkdir $WWW_PATH/$DOMAIN_NAME/public_html
 
 # move to the project directory
-cd $WWW_PATH/$PROJECT_NAME/public_html
+cd $WWW_PATH/$DOMAIN_NAME/public_html
 
-# download latest wordpress
+# download, decompress and move latest wordpress
 wget http://wordpress.org/latest.tar.gz
 tar xzf latest.tar.gz
 mv wordpress/* .
@@ -38,3 +43,4 @@ chown -R $WWW_USER:$WWW_GROUP .
 find . -type d -exec chmod 0755 {} \;
 find . -type f -exec chmod 0644 {} \;
 
+# so long and thanks for all the fish
